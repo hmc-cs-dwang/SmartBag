9a1c2e260098462a133d7a8d3de576f778858c0e
zhzhang2012 <zhzhangtony@gmail.com>
2016-04-15 23:57:24 -0700

    add launch screen

--- SmartBag/BluetoothConnectViewController.swift
+++ SmartBag/BluetoothConnectViewController.swift
@@ -7,3 +7,76 @@
 //
 
 import Foundation
+import UIKit
+
+class BluetoothConnectViewController : UIViewController, DFBlunoDelegate {
+    var blunoManager : DFBlunoManager?
+    var blunoDevice : DFBlunoDevice?
+    var discovered = false
+    
+    @IBOutlet weak var segueButton: UIButton!
+    @IBOutlet weak var loadingLabel: UILabel!
+    @IBOutlet weak var loadingIcon: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let bluno = DFBlunoManager.sharedInstance()
+        
+        if let blunoManager = bluno as? DFBlunoManager {
+            blunoManager.delegate = self
+            blunoManager.scan()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // DFBlunoDelegate functions
+    @objc func bleDidUpdateState(bleSupported: Bool) {
+        if (bleSupported) {
+            blunoManager!.scan()
+        }
+    }
+    
+    @objc func didDiscoverDevice(dev: DFBlunoDevice!) {
+        if (dev.identifier == "DFC48FB4-F866-E13C-BB90-9F9E14CA9BDE") {
+            loadingLabel.text = "Discovered My SmartBag!"
+            discovered = true
+            loadingIcon.hidden = true
+        } else {
+            loadingLabel.text = "Discovered a Unrecognized Device"
+            discovered = false
+        }
+    }
+    
+    @objc func readyToCommunicate(dev: DFBlunoDevice!) {
+        if (discovered) {
+            loadingLabel.text = "Connected to My SmartBag!"
+            blunoDevice = dev
+            segueButton.hidden = false
+        }
+    }
+    
+    @objc func didDisconnectDevice(dev: DFBlunoDevice!) {
+        if (discovered) {
+            loadingLabel.text = "Conenction is lost!"
+            blunoDevice = nil
+            segueButton.hidden = true;
+        }
+    }
+    
+    @objc func didWriteData(dev: DFBlunoDevice!) {
+        // pass
+    }
+    
+    @objc func didReceiveData(data: NSData!, device dev: DFBlunoDevice!) {
+        // pass
+    }
+
+    @IBAction func enterMainPage(sender: UIButton) {
+        self.performSegueWithIdentifier("enterMainViewSeque", sender: self)
+    }
+}
