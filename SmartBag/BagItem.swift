64e4535fb87f0b3cd308be1fb2339b3185ed609b
FernandoW08 <deronric@gmail.com>
2016-04-15 20:34:02 -0700

    bag item class

---
+++ SmartBag/BagItem.swift
@@ -0,0 +1,15 @@
+//
+//  BagItem.swift
+//  SmartBag
+//
+//  Created by Fernando Wang on 4/15/16.
+//  Copyright © 2016 Cam. All rights reserved.
+//
+
+import UIKit
+
+class BagItem: NSObject {
+    var name: String = ""
+    var itemImage: String = ""
+    var itemIn: Bool = false
+}
