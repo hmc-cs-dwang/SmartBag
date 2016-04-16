720324f2fa154566afef177f93db8ef108aba448
Cam Zhou <Cam@wl-195-100.wpa.claremont.edu>
2016-04-15 20:58:43 -0700

    Cam: Plist added

---
+++ SmartBag/PListManager.swift
@@ -0,0 +1,13 @@
+//
+//  PListManager.swift
+//  SmartBag
+//
+//  Created by Cam on 4/15/16.
+//  Copyright © 2016 Cam. All rights reserved.
+//
+
+import UIKit
+
+class PListManager: NSObject {
+    
+}
