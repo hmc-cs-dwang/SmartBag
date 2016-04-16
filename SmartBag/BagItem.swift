e5a2966ebc59bdd9196a8f74006cdbcd4e424528
Cam Zhou <Cam@wl-195-100.wpa.claremont.edu>
2016-04-15 22:09:39 -0700

    table modified, bag item modified

--- SmartBag/BagItem.swift
+++ SmartBag/BagItem.swift
@@ -12,11 +12,13 @@ class BagItem: NSObject {
     var name_ = ""
     var itemImage_ = ""
     var itemIn_ = false
+    var addTime_ = NSDate()
     
     init(name: String, itemImage: String, itemIn: Bool) {
         super.init()
         name_ = name
         itemImage_ = itemImage
         itemIn_ = itemIn
+        addTime_ = NSDate.init(timeIntervalSinceNow: 0)
     }
 }
